b55f2ceb12825103c7d6a75fcc5b4bf2ae6de377
itc-n24013 <n24013@std.it-college.ac.jp>
2025-10-10 02:01:32 +0000

    10 10 kadai

---
+++ m1_q3.py
@@ -0,0 +1,11 @@
+a = 12
+b = 5
+print(b - a) # (12)
+print(abs(-7)) # (13)
+print("(0:08)".format(64)) # (14)
+c = [2, 7, 15, 12, 9]
+c.sort(reverse=True)
+print(c) #(15)
+print(c[1:4] # (16)
+
+print(f'((a - b) / c[3]:_<8.1f)') 
