13fb8a78c18378c3543c701b96cfa57625caccdb
itc-n24013 <n24013@std.it-college.ac.jp>
2025-11-14 02:07:30 +0000

    11月14日

---
+++ m2_q4_19kadai.py
@@ -0,0 +1,5 @@
+x = int(input("x = "))
+x2 = x ** 2
+x3 = x ** 3
+print(x2)
+print(x3)
---
+++ m2_q4_20kadai.py
@@ -0,0 +1,7 @@
+list_b = []
+for n in range (5, 26):
+    if n % 2 == 1:
+        list_b.append(n)
+print(list_b)
+total = sum(list_b)
+print(total)
